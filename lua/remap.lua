25be75c1d1a448f2455ebc21cbaabd0bf71b6fec
Amith S <amiths12@outlook.com>
2023-11-29 18:10:02 +0530

    .zshrc

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,6 +1,7 @@
 -- function for transparency
 local vopt = vim.opt
 local keyset = vim.keymap.set
+vopt.nu = true
 vopt.rnu = true
 vopt.guicursor = ""
 vopt.tabstop = 4
@@ -11,36 +12,36 @@ vopt.smartindent = true
 
 vopt.wrap = true
 
-vopt.undodir=os.getenv("HOME") .. "/.vim/undo"
+vopt.undodir = os.getenv("HOME") .. "/.vim/undo"
 vopt.undofile = true
 vopt.hlsearch = false
 vopt.incsearch = true
 vopt.termguicolors = true
 
 vopt.updatetime = 250
-keyset('n','U','<cmd>redo<CR>')
-keyset('v','<M-j>',":m '>+1<CR>gv=gv")
-keyset('v','<M-k>',":m '<-2<CR>gv=gv")
+keyset('n', 'U', '<cmd>redo<CR>')
+keyset('v', '<M-j>', ":m '>+1<CR>gv=gv")
+keyset('v', '<M-k>', ":m '<-2<CR>gv=gv")
 
-keyset('n','<C-d>',"<C-d>zz")
-keyset('n','<C-u>',"<C-u>zz")
+keyset('n', '<C-d>', "<C-d>zz")
+keyset('n', '<C-u>', "<C-u>zz")
 
-keyset('n','n','nzzzv')
-keyset('n','N','nzzzv')
+keyset('n', 'n', 'nzzzv')
+keyset('n', 'N', 'nzzzv')
 
--- for this eg given below 
-keyset('x',"<leader>p","\"_dp")
+-- for this eg given below
+keyset('x', "<leader>p", "\"_dp")
 -- say we have foo and bar delete and paste over
-keyset('n','<leader>y',"\"+y")
-keyset('v','<leader>y',"\"+y")
-keyset('n','<leader>Y',"\"+Y")
+keyset('n', '<leader>y', "\"+y")
+keyset('v', '<leader>y', "\"+y")
+keyset('n', '<leader>Y', "\"+Y")
 
-keyset('n',"<leader>d","\"_d")
-keyset('v',"<leader>d","\"_d")
+keyset('n', "<leader>d", "\"_d")
+keyset('v', "<leader>d", "\"_d")
 
-keyset('i',"<C-c>","<Esc>")
+keyset('i', "<C-c>", "<Esc>")
 
-keyset('n',"<F3>", function()vim.lsp.buf.format{timeout = 20000} end)
+keyset('n', "<F3>", function() vim.lsp.buf.format { timeout = 20000 } end)
 
 keyset("n", "<C-k>", "<cmd>cnext<CR>zz")
 keyset("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -48,4 +49,16 @@ keyset("n", "<leader>k", "<cmd>lnext<CR>zz")
 keyset("n", "<leader>j", "<cmd>lprev<CR>zz")
 keyset("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-keyset('n','<C-c>','q!')
+keyset('n', '<C-c>', 'q!')
+
+-- Resize with arrows
+keyset("n", "<C-Up>", ":resize -2<CR>")
+keyset("n", "<C-Down>", ":resize +2<CR>")
+keyset("n", "<C-Left>", ":vertical resize -2<CR>")
+keyset("n", "<C-Right>", ":vertical resize +2<CR>")
+
+
+keyset("n", "<C-h>", "<C-w>h")
+keyset("n", "<C-j>", "<C-w>j")
+keyset("n", "<C-k>", "<C-w>k")
+keyset("n", "<C-l>", "<C-w>l")
